c2ebb6830dea526bc796e8881bb49cae9af26d5b
bigbanana80 <sepehra90@gmail.com>
2024-07-20 20:49:09 +0330

    Update forms.py

--- home/forms.py
+++ home/forms.py
@@ -1,8 +1,13 @@
 from django import forms
-from .models import contact
+from .models import contact , user
 
 
 class ContactForm(forms.ModelForm):
     class Meta:
         model = contact
         fields = ("name", "email", "subject", "message")
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = user
+        fields = ("username", "email", "password",)
