6a01e21c0f2c40f6fbf82363c3b58b506a653f45
bigbanana80 <sepehra90@gmail.com>
2024-08-01 19:14:02 +0330

    Update settings.py

--- Septo_PC/settings.py
+++ Septo_PC/settings.py
@@ -133,15 +133,15 @@ import environ
 env = environ.Env()
 environ.Env.read_env()
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = env('EMAIL_HOST')
 EMAIL_HOST_USER = env('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
-EMAIL_USE_TLS = False
 EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 EMAIL_HOST_PORT = 465
 
 DEFAULT_FROM_EMAIL = env("EMAIL_HOST")
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
  # configure it 
 #EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Custom setting. To email
