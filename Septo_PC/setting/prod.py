f3ad03ae883011207553ec1c8c037d9679e2f16f
bigbanana80 <sepehra90@gmail.com>
2024-09-21 19:13:42 +0330

    prod.py update

--- Septo_PC/setting/prod.py
+++ Septo_PC/setting/prod.py
@@ -3,15 +3,33 @@ from Septo_PC.settings import *
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-a&&a*^%#1zuv-8=3-+$(oe76@inmm_&qr8zox2x^17*k0%=6k1"
+from decouple import config
 
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+DEBUG = True 
 
 ALLOWED_HOSTS = []
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
-MEDIA_ROOT = BASE_DIR / "media"
+STATIC_ROOT = '/home/septupci/public_html/static'
+MEDIA_ROOT = '/home/septupci/public_html/media'
 
 CSRF_COOKIE_SECURE = True
+
+POSTGRES_DB = config("POSTGRES_DB") #database name
+POSTGRES_PASSWORD = config("POSTGRES_PASSWORD") # database user password
+POSTGRES_USER = config("POSTGRES_USER") # database username
+POSTGRES_HOST = config("POSTGRES_HOST") # database host
+POSTGRES_PORT = config("POSTGRES_PORT")
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": POSTGRES_DB,
+        "USER": POSTGRES_USER,
+        "PASSWORD": POSTGRES_PASSWORD,
+        "HOST": POSTGRES_HOST,
+        "PORT": POSTGRES_PORT,
+}}
