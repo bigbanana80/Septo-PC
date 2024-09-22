a39b21917e41991a67c2f571f0868811f4b3eee9
bigbanana80 <sepehra90@gmail.com>
2024-09-22 21:05:08 +0330

    better way to set it under construction

--- Septo_PC/urls.py
+++ Septo_PC/urls.py
@@ -16,13 +16,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include , re_path
 from django.conf import settings
 from django.conf.urls.static import static
-
+from home.views import not_available
 urlpatterns = [
     # path("admin/", admin.site.urls),
-    path("", include("home.urls")),
+    # path("", include("home.urls")),
+    re_path(r'^.*$', not_available)
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
