012b74b31b23e9b7db93530b5a5e044cd083ca63
bigbanana80 <sepehra90@gmail.com>
2024-09-22 20:15:11 +0330

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path , re_path
 from django.contrib.auth.views import (
     LogoutView, 
     PasswordResetView, 
@@ -28,4 +28,5 @@ urlpatterns = [
     path('password-reset/done/', PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'),name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'),name='password_reset_confirm'),
     path('password-reset-complete/',PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'),name='password_reset_complete'),
+    # re_path(r'^.*$', views.not_available) use this to make the website not available temporary
 ]
